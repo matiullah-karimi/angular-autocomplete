53107d1309f46bbb5f1269a6f01ea9f748297e1d
Matiullah Karimi <matiullah.karimi@gmail.com>
2020-10-07 13:20:10 +0430

    Add input mask module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-input-mask';
+
+  text: string;
+
+    results: string[];
+
+    search(event) {
+        console.log(event)
+    }
+
+    handleDropdown(event) {
+        //event.query = current value in input field
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputMaskModule } from 'src/input-mask';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputMaskModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
